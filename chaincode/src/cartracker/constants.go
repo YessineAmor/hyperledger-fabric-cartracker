cdb39f50496ee216b5bf9631b760860d867dd2f3
Yassine <yassine.amor@esprit.tn>
2019-08-26 17:06:08 +0100

    create constants.go

---
+++ chaincode/src/cartracker/constants.go
@@ -0,0 +1,14 @@
+package cartracker
+
+const (
+	ManufacturerOrgMSP             = "ManufacturerOrgMSP"
+	ManufacturerOrgCA              = "ca.manufacturerorg.cartracker.com"
+	DealershipOrgMSP               = "DealershipOrgMSP"
+	DealershipOrgCA                = "ca.dealershiporg.cartracker.com"
+	InsuranceOrgMSP                = "InsuranceOrgMSP"
+	InsuranceOrgCA                 = "ca.insuranceorg.cartracker.com"
+	CarRegistrationAuthorityOrgMSP = "CarRegistrationAuthorityOrgMSP"
+	CarRegistrationAuthorityOrgCA  = "ca.carregistrationauthorityorg.cartracker.com"
+	BuyerOrgMSP                    = "BuyerOrgMSP"
+	BuyerOrgCA                     = "ca.buyerorg.cartracker.com"
+)
